370122019df45f02c5565551167492702b65dac7
Noah Kim <noahmk1@uci.edu>
2024-03-30 21:34:20 -0700

    change header styling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,9 @@
 export default function Header() {
 	return (
-		<div>
+		<div className='d-flex gap-3 p-4 align-items-center'>
 			{/* TODO: include an icon for the quote book */}
-			<img src="/quotebook.png" alt="" /><h1>Hack at UCI Tech Deliverable</h1>
+			<img src="/quotebook.png" alt="Quotebook Logo" height='75px' />
+			<h1 className='display-4'>Hack at UCI Quotebook</h1>
 		</div>
 	)
 }
