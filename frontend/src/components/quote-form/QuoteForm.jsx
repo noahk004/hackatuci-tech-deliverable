3ffb253ee3b406ff7cc090efd3b9d190ebe7f2b9
Noah Kim <noahmk1@uci.edu>
2024-04-01 12:10:21 -0700

    slight changes to quote form ui

--- frontend/src/components/quote-form/QuoteForm.jsx
+++ frontend/src/components/quote-form/QuoteForm.jsx
@@ -2,6 +2,8 @@ import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
 
+import './QuoteForm.css'
+
 export default function QuoteForm({ active, handleClose }) {
 	return (
 		<div>
@@ -12,18 +14,18 @@ export default function QuoteForm({ active, handleClose }) {
 				<Modal.Body>
 					<Form>
 						<Form.Group>
-							<Form.Label>Name</Form.Label>
+							<Form.Label>Name:</Form.Label>
 							<Form.Control placeholder='Peter the Anteater' />
 						</Form.Group>
 						<Form.Group className='mt-2'>
-							<Form.Label>Enter your quote:</Form.Label>
-							<Form.Control as='textarea' rows={3} />
+							<Form.Label>Message:</Form.Label>
+							<Form.Control as='textarea' rows={3} placeholder='Zot Zot Zot!' />
 						</Form.Group>
 					</Form>
 				</Modal.Body>
 				<Modal.Footer>
 					<Button onClick={handleClose} variant='secondary'>Close</Button>
-					<Button>Submit</Button>
+					<Button className='quote-submit'>Submit</Button>
 				</Modal.Footer>
 			</Modal>
 		</div>
