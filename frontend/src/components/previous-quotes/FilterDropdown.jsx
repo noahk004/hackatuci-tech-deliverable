25b5c644660229aa9e053df8ec662a5d7cefda10
Noah Kim <noahmk1@uci.edu>
2024-04-08 22:07:07 -0700

    add functionality for filter

--- frontend/src/components/previous-quotes/FilterDropdown.jsx
+++ frontend/src/components/previous-quotes/FilterDropdown.jsx
@@ -1,6 +1,7 @@
 import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
-export default function FilterDropdown() {
+export default function FilterDropdown({ changeFilter }) {
     return (
         <Dropdown className='mb-3'>
             <Dropdown.Toggle variant='secondary'>
@@ -8,10 +9,10 @@ export default function FilterDropdown() {
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
-                <Dropdown.Item>All</Dropdown.Item>
-                <Dropdown.Item>Last week</Dropdown.Item>
-                <Dropdown.Item>Last month</Dropdown.Item>
-                <Dropdown.Item>Last year</Dropdown.Item>
+                <Dropdown.Item onClick={changeFilter(0)}>All</Dropdown.Item>
+                <Dropdown.Item onClick={changeFilter(1)}>Last week</Dropdown.Item>
+                <Dropdown.Item onClick={changeFilter(2)}>Last month</Dropdown.Item>
+                <Dropdown.Item onClick={changeFilter(3)}>Last year</Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     )
--- frontend/src/components/previous-quotes/PreviousQuotes.jsx
+++ frontend/src/components/previous-quotes/PreviousQuotes.jsx
@@ -6,11 +6,42 @@ import FilterDropdown from './FilterDropdown.jsx'
 
 import './PreviousQuotes.css'
 
+
 export default function PreviousQuotes() {
     const [quoteData, setQuoteData] = useState(null);
 
     const [selectedQuote, setSelectedQuote] = useState(null);
     const [showQuote, setShowQuote] = useState(false);
+
+    const filters = {
+        ALL: 0,
+        LASTWEEK: 1,
+        LASTMONTH: 2,
+        LASTYEAR: 3
+    }
+    const [filterBy, setFilterBy] = useState(filters.ALL)
+    
+    const changeFilter = (filter) => {
+        return () => setFilterBy(filter)
+    }
+
+    const getFilterFunc = (filterBy) => {
+        let startTime = (new Date()).getTime();
+        if (filterBy === filters.ALL) {
+            startTime = 0;
+        } else if (filterBy === filters.LASTWEEK) {
+            startTime -= 7*24*60*60*1000;
+        } else if (filterBy === filters.LASTMONTH) {
+            startTime -= 30*24*60*60*1000;
+        } else if (filterBy === filters.LASTYEAR) {
+            startTime -= 365*24*60*60*1000;
+        }
+        const sorter = (e) => {
+            let eDate = (new Date(e.time)).getTime();
+            return (eDate >= startTime ? true : false);
+        }
+        return sorter;
+    }
 
     const handleCardClick = (card) => {
         setSelectedQuote(card)
@@ -20,16 +51,16 @@ export default function PreviousQuotes() {
     useEffect(() => {
         fetch('http://localhost:8000/allquotes')
             .then(res => res.json())
-            .then(json => setQuoteData(json))
+            .then(json => {setQuoteData(json.sort().reverse()); console.log(json)})
             .catch(error => console.log(error))
     }, [])
 
     return (
         <div className='m-4'>
             <h2 className='display-6 mb-3'>Previous Quotes</h2>
-            <FilterDropdown />
+            <FilterDropdown changeFilter={changeFilter}/>
             <div className='d-flex flex-wrap gap-3'>
-                {quoteData?.map((card, index) => (
+                {quoteData?.filter(getFilterFunc(filterBy)).map((card, index) => (
                     <QuoteCard key={index} onClick={() => handleCardClick(card)} name={card.name} message={card.message} time={card.time} />
                 ))}
             </div>
